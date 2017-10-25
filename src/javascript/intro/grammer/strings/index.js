29691073f31619cb6882b3b0c8141b436cda51d9
kelsrwalker <kelsrwalker@yahoo.com>
2017-10-25 15:53:00 -0600

    complete

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,13 @@
-export const greeting = '';
+export const greeting = 'Hello World';
+
+export const characterAt = greeting.charAt(3);
+
+export const greetingArray = greeting.split(" ");
+
+const begins = (str, char) => {
+  return str[0] === char
+};
+
+const beginsInsensitive = (str, char) => {
+  return str[].toLowerCase() === char.toLowerCase()
+};
